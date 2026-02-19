7eb52d7a5de05e8c2cde012ee6926aa6f907f840
Shashank Chilakapati <shashank.chilakapati@peopletech.com>
2026-02-19 10:59:30 -0800

    Automate Lambda Layer deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type = "string"
+  default = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type = "string"
+  default = "MyLambda"
+}
+
+variable "lambda_handler" {
+  description = "Handler for the Lambda function"
+  type = "string"
+  default = "app.handler"
+}
+
+variable "lambda_runtime" {
+  description = "Runtime for the Lambda function"
+  type = "string"
+  default = "python3.9"
+}
+
+variable "lambda_zip_path" {
+  description = "Path to lambda zip function"
+  type = "string"
+  default = "../lambda.zip"
+}
+
+variable "layer_name" {
+  description = "Name of Lambda Layer"
+  type = "string"
+  default = "utils-layer"
+}
+
+variable "layer_zip_path" {
+  description = "Path to lambda layer zip file"
+  type = "string"
+  default = "../layer.zip"
+}
