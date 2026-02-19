408cd65df38262ad561115ee57c28334bd97ba0f
Shashank Chilakapati <shashank.chilakapati@peopletech.com>
2026-02-19 11:22:02 -0800

    Automate Lambda Layer deployment

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,11 +1,15 @@
-resource "aws_lambda_function" "my_lambda" {
-  function_name = "MyLambda"
-  role          = aws_iam_role.lambda_exec.arn
-  handler       = "app.handler"
-  runtime       = "python3.9"
-  filename      = "${path.module}/../lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/../lambda.zip")
-  layers        = [aws_lambda_layer_version.utils_layer.arn]
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../lambda"    # relative to terraform folder
+  output_path = "lambda.zip"
+}
 
-  
+resource "aws_lambda_function" "my_lambda" {
+  function_name      = "MyLambda"
+  role               = aws_iam_role.lambda_exec.arn
+  handler            = "app.handler"
+  runtime            = "python3.9"
+  filename           = data.archive_file.lambda_zip.output_path
+  source_code_hash   = data.archive_file.lambda_zip.output_base64sha256
+  layers             = [aws_lambda_layer_version.utils_layer.arn]
 }
